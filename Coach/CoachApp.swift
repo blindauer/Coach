91edc392d4b83c0df0f9a76bcf5194864d9fa904
Bradley Lindauer <blindauer@mac.com>
2021-03-24 21:28:41 -0700

    Initial Commit

---
+++ Coach/CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachApp.swift
+//  Coach
+//
+//  Created by Bradley Lindauer on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
